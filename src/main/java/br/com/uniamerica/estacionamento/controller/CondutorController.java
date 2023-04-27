3e518fef1886ac5bb95b72dc100bfc4d6f346fda
Bouchra Akl <bushraakl1234@gmail.com>
2023-04-27 13:33:23 -0300

    Condutor Repo + Controller Done

--- src/main/java/br/com/uniamerica/estacionamento/controller/CondutorController.java
+++ src/main/java/br/com/uniamerica/estacionamento/controller/CondutorController.java
@@ -1,4 +1,77 @@
+/* -------------------Package--------------------------- */
 package br.com.uniamerica.estacionamento.controller;
 
+/* -------------------Imports--------------------------- */
+
+import br.com.uniamerica.estacionamento.entity.Condutor;
+import br.com.uniamerica.estacionamento.repository.CondutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/* ----------------------------------------------------- */
+@Controller
+@RequestMapping(value = "api/condutor")
 public class CondutorController {
+
+    @Autowired
+    private CondutorRepository condutorRepository;
+
+    /* -------------------get by id--------------------------- */
+    @GetMapping
+    public ResponseEntity<?> findByIdRequest(@RequestParam("id") final Long id) {
+        final Condutor condutor = this.condutorRepository.findById(id).orElse(null);
+        return condutor == null ? ResponseEntity.badRequest().body("ID não encontrado") : ResponseEntity.ok(condutor);
+    }
+
+    /* -------------------get by all--------------------------- */
+    @GetMapping("/all")
+    public ResponseEntity<?> findByAllRequest() {
+        return ResponseEntity.ok(this.condutorRepository.findAll());
+    }
+
+    /* -------------------get by ativo--------------------------- */
+    @GetMapping("/ativos")
+    public ResponseEntity<?> findCondutoresAtivos() {
+        return ResponseEntity.ok(this.condutorRepository.findAllByActive(true));
+    }
+
+    /* -------------------post--------------------------- */
+    @PostMapping
+    public ResponseEntity<?> cadastrarCondutor(@RequestBody final Condutor condutor) {
+        try {
+            this.condutorRepository.save(condutor);
+            return ResponseEntity.ok("Condutor Cadastrado com Sucesso");
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("ERRO :" + e.getCause().getCause().getMessage());
+        }
+    }
+
+    /* -------------------put--------------------------- */
+    @PutMapping
+    public ResponseEntity<?> editarCondutor(
+            @RequestParam("id") final Long id,
+            @RequestBody final Condutor condutor
+    ) {
+        try {
+            final Condutor condutorBanco = this.condutorRepository.findById(id).orElse(null);
+            if (condutorBanco == null) {
+                throw new RuntimeException("Nao foi possivel identificar o Registro informado");
+            }
+            this.condutorRepository.save(condutor);
+            return ResponseEntity.ok("Registro atualizado com sucesso");
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.internalServerError().body("Error: " + e.getCause().getCause().getMessage());
+        } catch (RuntimeException e) {
+            return ResponseEntity.internalServerError().body("Error: " + e.getMessage());
+        }
+    }
+
+    /* -------------------delete--------------------------- */
+
+
+
+
 }
--- src/main/java/br/com/uniamerica/estacionamento/entity/Condutor.java
+++ src/main/java/br/com/uniamerica/estacionamento/entity/Condutor.java
@@ -6,14 +6,21 @@ package br.com.uniamerica.estacionamento.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.envers.AuditTable;
+import org.hibernate.envers.Audited;
 
 import java.time.LocalTime;
 
 /* ----------------------------------------------------- */
 @Entity
+@Audited
 @Table(name = "condutores", schema = "public")
+@AuditTable(value = "condutores_audit", schema = "audit")
 public class Condutor extends AbstractEntity {
 
+    @Getter @Setter
     @Column(name = "nome", nullable = false, length = 100)
     private String nome;
 

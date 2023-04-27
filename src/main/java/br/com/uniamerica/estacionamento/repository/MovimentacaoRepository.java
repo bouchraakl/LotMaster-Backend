5f1e1759f9f0724e2eac25cbff7337c23b675108
Bouchra Akl <bushraakl1234@gmail.com>
2023-04-27 13:38:18 -0300

    Movimentacao Repo + Controller Done

--- src/main/java/br/com/uniamerica/estacionamento/repository/MovimentacaoRepository.java
+++ src/main/java/br/com/uniamerica/estacionamento/repository/MovimentacaoRepository.java
@@ -6,6 +6,7 @@ package br.com.uniamerica.estacionamento.repository;
 import br.com.uniamerica.estacionamento.entity.Movimentacao;
 import br.com.uniamerica.estacionamento.entity.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 import java.util.List;
@@ -13,5 +14,8 @@ import java.util.List;
 /* ----------------------------------------------------- */
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long> {
     public List<Movimentacao> findByVeiculo(final Veiculo veiculo);
+
+    @Query(value = "SELECT * FROM movimentacoes mov WHERE mov.entrada IS NOT NULL AND mov.saida IS NULL")
+    public List<Movimentacao> findByMovAbertas();
 
 }

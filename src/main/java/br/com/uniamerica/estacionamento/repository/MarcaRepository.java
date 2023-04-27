7699228bb2a5c2292d42d4ff248f1ad19c23c603
Bouchra Akl <bushraakl1234@gmail.com>
2023-04-27 13:39:14 -0300

    Marca Repo + Controller Done

--- src/main/java/br/com/uniamerica/estacionamento/repository/MarcaRepository.java
+++ src/main/java/br/com/uniamerica/estacionamento/repository/MarcaRepository.java
@@ -7,12 +7,15 @@ import br.com.uniamerica.estacionamento.entity.Marca;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 /* ----------------------------------------------------- */
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
     public List<Marca> findByNome(final String nome);
+
+    @Query(value = "select * from marcas where ativo = :true", nativeQuery = true)
+    public List<Marca> findAllByActive(@Param("true") final boolean ativo);
 
 }

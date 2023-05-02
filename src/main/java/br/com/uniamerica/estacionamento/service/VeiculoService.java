b9a3abefb952b106d1df5c9c44e8ef5f4f3deb25
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-02 20:16:45 -0300

    Service Structure Established

--- src/main/java/br/com/uniamerica/estacionamento/service/VeiculoService.java
+++ src/main/java/br/com/uniamerica/estacionamento/service/VeiculoService.java
@@ -2,9 +2,9 @@
 package br.com.uniamerica.estacionamento.service;
 
 //------------------Imports----------------------
-
-
+import org.springframework.stereotype.Service;
 
 //------------------------------------------------
+@Service
 public class VeiculoService {
 }

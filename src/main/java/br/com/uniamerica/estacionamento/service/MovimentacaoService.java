c1300f0836ec57b31d2df16821267758884774af
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-06 12:56:10 -0300

    MovimentacaoService | added call for veiculo and condutor validations

--- src/main/java/br/com/uniamerica/estacionamento/service/MovimentacaoService.java
+++ src/main/java/br/com/uniamerica/estacionamento/service/MovimentacaoService.java
@@ -28,35 +28,38 @@ public class MovimentacaoService {
     @Autowired
     private VeiculoService veiculoService;
 
+    @Autowired
+    private CondutorService condutorService;
+
     @Transactional(readOnly = true, rollbackFor = Exception.class)
     public void validarCadastroMovimentacao(Movimentacao movimentacao) {
 
-        // Verificar se a movimentação já existe
-        Assert.notNull(movimentacao.getId(), "Movimentação não cadastrada no banco de dados.");
+        // Verificar se a ID do veículo já existe
+        if (!movimentacaoRepository.existsById(movimentacao.getId())) {
+            throw new IllegalArgumentException("A ID do veículo não existe no banco de dados: " + movimentacao.getId());
+        }
 
-        // Verificar se a placa do veículo informado é válido
-        this.veiculoService.validarPlaca(movimentacao.getVeiculo().getPlaca());
+        // Executar validações no objeto do veículo
+        veiculoService.validarCadastroVeiculo(movimentacao.getVeiculo());
 
-        // Verificar se o CPF do cliente informado está cadastrado no sistema é valido
-        final String cpfFormat = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}";
-        Assert.isTrue(movimentacao.getCondutor().getCpf().matches(cpfFormat), "CPF inválido!");
+        // Executar validações no objeto do condutor
+        condutorService.validarCadastroCondutor(movimentacao.getCondutor());
 
         // Verificar se a movimentação está dentro do horário de funcionamento do estacionamento
         LocalTime inicioExpediente = LocalTime.parse("08:00:00");
         LocalTime fimExpediente = LocalTime.parse("18:00:00");
-
-        LocalDateTime entrada = LocalDateTime.now();
-        LocalTime horarioMovimentacao = entrada.toLocalTime();
-        Assert.isTrue(horarioMovimentacao.isAfter(inicioExpediente)
-                        && horarioMovimentacao.isBefore(fimExpediente),
-                "Movimentação fora do horário de funcionamento do estacionamento.");
+        LocalTime horarioMovimentacao = LocalTime.now().withNano(0);
+        if (horarioMovimentacao.isBefore(inicioExpediente) || horarioMovimentacao.isAfter(fimExpediente)) {
+            throw new IllegalArgumentException("A movimentação está fora do horário de funcionamento do estacionamento.");
+        }
 
         // Verificar se há vagas disponíveis para o tipo de veículo informado
-        int numeroVagas = this.configuracaoRepository.countByTipo(movimentacao.getVeiculo().getTipo());
+        long numeroVagas = configuracaoRepository.countByTipo(movimentacao.getVeiculo().getTipo());
         if (numeroVagas == 0) {
             throw new IllegalArgumentException("Não há vagas disponíveis para o tipo de veículo informado.");
         }
     }
+
 
     @Transactional(readOnly = true, rollbackFor = Exception.class)
     public void validarUpdateMovimentacao(Movimentacao movimentacao) {
@@ -75,5 +78,10 @@ public class MovimentacaoService {
         Assert.notNull(movimentacao.getSaida(), "Data de entrada não informada.");
         Assert.notNull(movimentacao.getVeiculo(), "Veículo não informado.");
         Assert.notNull(movimentacao.getCondutor(), "Condutor não informado.");
+    }
+
+    public void validarPlaca(String placa) {
+        final String PADRAO_REGEX_PLACA_BRASILEIRA = "^[A-Z]{3}\\d{4}$";
+        Assert.isTrue(placa.matches(PADRAO_REGEX_PLACA_BRASILEIRA), "Placa inválida.");
     }
 }

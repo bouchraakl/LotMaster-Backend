2d9a2ef90bdd1aa0b21f2453f518726bb253d7e8
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-06 12:22:34 -0300

    CondutorRepository | added search condutor by cpf

--- src/main/java/br/com/uniamerica/estacionamento/repository/CondutorRepository.java
+++ src/main/java/br/com/uniamerica/estacionamento/repository/CondutorRepository.java
@@ -14,6 +14,9 @@ import java.util.List;
 public interface CondutorRepository extends JpaRepository<Condutor, Long> {
     public List<Condutor> findByNome(final String nome);
 
+    @Query(value = "SELECT * FROM condutores WHERE cpf like :cpf",nativeQuery = true)
+    public List<Condutor> findbyCPF(@Param("cpf") final String cpf);
+
     @Query(value = "SELECT * FROM condutores WHERE ativo like :true", nativeQuery = true)
     public List<Condutor> findAllByActive(@Param("true") final boolean ativo);
 

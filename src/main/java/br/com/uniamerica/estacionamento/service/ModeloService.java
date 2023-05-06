d09a8fc42c52513f91f40ce2d4411642755605d6
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-06 11:57:59 -0300

    ModeloService | updated to validate more specific cases

--- src/main/java/br/com/uniamerica/estacionamento/service/ModeloService.java
+++ src/main/java/br/com/uniamerica/estacionamento/service/ModeloService.java
@@ -26,6 +26,7 @@ public class ModeloService {
 
     @Transactional(readOnly = true, rollbackFor = Exception.class)
     public void validarCadastroModelo(Modelo modelo) {
+
         // Verificar se o nome está informado
         Assert.notNull(modelo.getNome(), "Nome do modelo não informado!");
 
@@ -36,6 +37,10 @@ public class ModeloService {
         final List<Modelo> modelosByNome = this.modeloRepository.findByNome(modelo.getNome());
         Assert.isTrue(modelosByNome.isEmpty(), "nome do modelo já cadastrado");
 
+        // Verificar se o nome do marca já existe
+        final List<Marca> marcasByNome = this.marcaRepository.findByNome(modelo.getMarca().getNome());
+        Assert.isTrue(marcasByNome.isEmpty(), "nome da marca já cadastrado");
+
         // Verificar se a marca já foi cadastrada
         final Marca marca = this.marcaRepository.findById(modelo.getMarca().getId()).orElse(null);
         Assert.isNull(marca, "Objeto marca já cadastrado no banco de dados");
@@ -43,8 +48,17 @@ public class ModeloService {
 
     @Transactional(readOnly = true, rollbackFor = Exception.class)
     public void validarUpdateModelo(Modelo modelo) {
-        // Verificar se o ID do modelo não é nulo
-        Assert.notNull(modelo.getId(), "Objeto modelo não encontrado no banco de dados");
+
+//        // Verificar se o ID do modelo não é nulo
+//        Assert.notNull(modelo.getId(), "Objeto modelo não encontrado no banco de dados");
+
+        // Verificar se o nome do modelo já existe
+        final List<Modelo> modelosByNome = this.modeloRepository.findByNome(modelo.getNome());
+        Assert.isTrue(modelosByNome.isEmpty(), "nome do modelo já cadastrado");
+
+        // Verificar se o nome do marca já existe
+        final List<Marca> marcasByNome = this.marcaRepository.findByNome(modelo.getMarca().getNome());
+        Assert.isTrue(marcasByNome.isEmpty(), "nome da marca já cadastrado");
 
         // Verificar se o ID da marca não é nulo
         Assert.notNull(modelo.getMarca().getId(), "Objeto marca não encontrado no banco de dados");
@@ -52,6 +66,13 @@ public class ModeloService {
         // Verificar se os campos obrigatórios foram preenchidos
         Assert.notNull(modelo.getNome(), "Nome do modelo não informado");
         Assert.notNull(modelo.getCadastro(), "Data de cadastro do modelo não informada");
+    }
+
+    @Transactional(readOnly = true,rollbackFor = Exception.class)
+    public void validarDeleteModelo(Modelo modelo){
+
+        // Verificar se o ID do modelo existe
+        Assert.notNull(modelo.getId(),"ID modelo não existe no banco de dados");
     }
 
 

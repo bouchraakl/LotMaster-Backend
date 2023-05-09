49a8252c65426ab957440ad47ea3219ebf6d8838
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-09 20:13:15 -0300

    Cor and Tipo | String format correction

--- src/main/java/br/com/uniamerica/estacionamento/entity/Cor.java
+++ src/main/java/br/com/uniamerica/estacionamento/entity/Cor.java
@@ -3,10 +3,10 @@ package br.com.uniamerica.estacionamento.entity;
 
 /* ----------------------------------------------------- */
 public enum Cor {
-    Prata,
-    Cinza,
-    Branco,
-    Preto,
-    Rosa,
-    Azul
+    PRATA,
+    CINZA,
+    BRANCO,
+    PRETO,
+    ROSA,
+    AZUL
 }
--- src/main/java/br/com/uniamerica/estacionamento/entity/Tipo.java
+++ src/main/java/br/com/uniamerica/estacionamento/entity/Tipo.java
@@ -3,7 +3,7 @@ package br.com.uniamerica.estacionamento.entity;
 
 /* ----------------------------------------------------- */
 public enum Tipo {
-    Carro,
-    Moto,
-    Van
+    CARRO,
+    MOTO,
+    VAN
 }

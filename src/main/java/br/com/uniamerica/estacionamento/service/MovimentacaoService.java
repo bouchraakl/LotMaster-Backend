338139904ebc4c2cf858ace8194f2a87e4b3afc4
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-16 19:03:58 -0300

    MovimentacaoService.java | Applying clean code principles and calculating valorMulta and tempoMulta

--- src/main/java/br/com/uniamerica/estacionamento/service/MovimentacaoService.java
+++ src/main/java/br/com/uniamerica/estacionamento/service/MovimentacaoService.java
@@ -3,19 +3,25 @@ package br.com.uniamerica.estacionamento.service;
 
 //------------------Imports----------------------
 
+import br.com.uniamerica.estacionamento.entity.Condutor;
+import br.com.uniamerica.estacionamento.entity.Configuracao;
 import br.com.uniamerica.estacionamento.entity.Movimentacao;
+import br.com.uniamerica.estacionamento.entity.Veiculo;
 import br.com.uniamerica.estacionamento.repository.CondutorRepository;
 import br.com.uniamerica.estacionamento.repository.ConfiguracaoRepository;
 import br.com.uniamerica.estacionamento.repository.MovimentacaoRepository;
 import br.com.uniamerica.estacionamento.repository.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+
 
 /*
 - Essa classe é responsável por realizar validações de dados relacionados a movimentacoes.
@@ -24,11 +30,6 @@ import java.time.LocalTime;
 */
 @Service
 public class MovimentacaoService {
-
-    // Declaração das constantes OPENING_TIME, CLOSING_TIME e CURRENT_TIME
-    private static final LocalTime OPENING_TIME = LocalTime.parse("08:00:00");
-    private static final LocalTime CLOSING_TIME = LocalTime.parse("18:00:00");
-    private static final LocalTime CURRENT_TIME = LocalTime.now().withNano(0);
 
     @Autowired
     private MovimentacaoRepository movimentacaoRepository;
@@ -39,6 +40,13 @@ public class MovimentacaoService {
     @Autowired
     private ConfiguracaoRepository configuracaoRepository;
 
+
+    private Configuracao obterConfiguracao() {
+        assert configuracaoRepository != null;
+        return configuracaoRepository.findById(1L).orElse(null);
+    }
+
+
     /**
      * Realiza a validação dos dados necessários para cadastrar uma movimentação de veículo no estacionamento.
      *
@@ -47,62 +55,26 @@ public class MovimentacaoService {
     @Transactional(readOnly = true, rollbackFor = Exception.class)
     public void validarCadastroMovimentacao(Movimentacao movimentacao) {
 
-        // Verifica se o veículo está ativo
-        Assert.isTrue(movimentacao.getVeiculo().isAtivo(),
-                "O veículo associado a essa movimentação está inativo. " +
-                        "Por favor, verifique o status do veículo e tente novamente.");
+        Configuracao configuracao = obterConfiguracao();
 
-        // Verifica se o objeto veículo foi informado
-        Assert.notNull(movimentacao.getVeiculo(),
-                "O objeto veículo não foi informado." +
-                        " Por favor, preencha todas as informações obrigatórias para prosseguir com a movimentação.");
+        BigDecimal valorMinutoMulta = configuracao.getValorMinutoMulta();
+        movimentacao.setValorHoraMulta(valorMinutoMulta.multiply(new BigDecimal("60.0")));
 
-        // Verifica se o ID do veículo existe no banco de dados
-        Assert.isTrue(this.veiculoRepository.existsById(movimentacao.getVeiculo().getId()),
-                "Não foi possível registrar a movimentação, " +
-                        "o veículo informado não foi encontrado no sistema.");
-
-        // Verifica se o ID do veículo foi informado
-        Assert.notNull(movimentacao.getVeiculo().getId(),
-                "O ID do veículo em movimentação não pode ser nulo.");
-
-        // Verifica se o condutor está ativo
-        Assert.isTrue(movimentacao.getCondutor().isAtivo(),
-                "O condutor associado a essa movimentação está inativo." +
-                        " Por favor, verifique o status do condutor e tente novamente.");
-
-        // Verifica se o objeto condutor foi informado
-        Assert.notNull(movimentacao.getCondutor(),
-                "O objeto condutor não foi informado. " +
-                        "Por favor, preencha todas as informações obrigatórias para prosseguir com a movimentação.");
-
-        // Verifica se o ID do condutor existe no banco de dados
-        Assert.isTrue(this.condutorRepository.existsById(movimentacao.getCondutor().getId()),
-                "Não foi possível registrar a movimentação," +
-                        " o condutor informado não foi encontrado no sistema.");
-
-        // Verifica se o ID do condutor foi informado
-        Assert.notNull(movimentacao.getCondutor().getId(),
-                "O ID do condutor em movimentação não pode ser nulo.");
-
-        movimentacao.setEntrada(LocalDateTime.now());
-
-        // Verifica se a movimentação está dentro do horário de funcionamento do estacionamento
-        Assert.isTrue(!CURRENT_TIME.isBefore(OPENING_TIME) || CURRENT_TIME.isAfter(CLOSING_TIME),
-                "Erro: Horário inválido. O horário atual está fora do intervalo de funcionamento permitido. " +
-                        "Horário de funcionamento: das " + OPENING_TIME + " às " + CLOSING_TIME + ".");
+        movimentacao.setCadastro(LocalDateTime.now());
+        validarMovimentacao(movimentacao);
 
         if (movimentacao.getSaida() != null) {
-            LocalDateTime entrada = movimentacao.getEntrada();
-            LocalDateTime saida = movimentacao.getSaida();
-            Duration duracao = Duration.between(entrada.toLocalTime(), saida.toLocalTime());
-            LocalTime duracaoLocalTime = duracao.toMinutes() >= 0 ? LocalTime.MIDNIGHT.plus(duracao) : LocalTime.MIDNIGHT.minus(duracao);
-            movimentacao.setTempo(duracaoLocalTime);
-        }else{
-            movimentacao.setTempo(LocalTime.MIDNIGHT);
+
+            Assert.isTrue(movimentacao.getEntrada().isBefore(movimentacao.getSaida()),
+                    "O tempo de entrada deve ser posterior ao tempo de saída.");
+
+            saidaOperations(movimentacao);
+
+        } else {
+            configurarValoresPadrao(movimentacao);
         }
 
-
+        movimentacao.setValorHora(configuracao.getValorHora());
     }
 
     /**
@@ -113,66 +85,27 @@ public class MovimentacaoService {
     @Transactional(readOnly = true, rollbackFor = Exception.class)
     public void validarUpdateMovimentacao(Movimentacao movimentacao) {
 
-        // Verifica se o ID da movimentação foi informado
-        Assert.notNull(movimentacao.getId(),
-                "O ID da movimentação fornecida é nulo." +
-                        " Certifique-se de que o objeto de movimentação tenha " +
-                        "um ID válido antes de realizar essa operação.");
+        movimentacao.setAtualizacao(LocalDateTime.now());
 
-        // Verifica se o veículo associado à movimentação está ativo
-        Assert.isTrue(movimentacao.getVeiculo().isAtivo(),
-                "O veículo associado a essa movimentação está inativo. " +
-                        "Por favor, verifique o status do veículo e tente novamente.");
+        Assert.notNull(movimentacao.getId(), "O ID da movimentação fornecida é nulo.");
 
-        // Verifica se o ID do veículo foi informado
-        Assert.notNull(movimentacao.getVeiculo().getId(),
-                "O ID do veículo em movimentação não pode ser nulo.");
+        Assert.isTrue(movimentacaoRepository.existsById(movimentacao.getId()),
+                "O ID da movimentação especificada não foi encontrada na base de dados.");
 
-        // Verifica se o objeto do veículo foi informado
-        Assert.notNull(movimentacao.getVeiculo(),
-                "O objeto veículo não foi informado." +
-                        " Por favor, preencha todas as informações obrigatórias para prosseguir com a movimentação.");
-
-        // Verifica se o ID do veículo existe no banco de dados
-        Assert.isTrue(this.veiculoRepository.existsById(movimentacao.getVeiculo().getId()),
-                "Não foi possível registrar a movimentação, " +
-                        "o veículo informado não foi encontrado no sistema.");
-
-        // Verifica se o condutor associado à movimentação está ativo
-        Assert.isTrue(movimentacao.getCondutor().isAtivo(),
-                "O condutor associado a essa movimentação está inativo." +
-                        " Por favor, verifique o status do condutor e tente novamente.");
-
-        // Verifica se o ID do condutor foi informado
-        Assert.notNull(movimentacao.getCondutor().getId(),
-                "O ID do condutor em movimentação não pode ser nulo.");
-
-        // Verifica se o objeto do condutor foi informado
-        Assert.notNull(movimentacao.getCondutor(),
-                "O objeto condutor não foi informado. " +
-                        "Por favor, preencha todas as informações obrigatórias para prosseguir com a movimentação.");
-
-        // Verifica se o ID do condutor existe no banco de dados
-        Assert.isTrue(this.condutorRepository.existsById(movimentacao.getCondutor().getId()),
-                "Não foi possível registrar a movimentação," +
-                        " o condutor informado não foi encontrado no sistema.");
-
-        // Verifica se a data de entrada da movimentação foi informada
+        validarVeiculo(movimentacao.getVeiculo());
+        validarCondutor(movimentacao.getCondutor());
         Assert.notNull(movimentacao.getEntrada(), "A data de entrada da movimentação não foi informada.");
 
-        // Verifica se a movimentação está dentro do horário de funcionamento do estacionamento
-        Assert.isTrue(!CURRENT_TIME.isBefore(OPENING_TIME) || CURRENT_TIME.isAfter(CLOSING_TIME),
-                "Erro: Horário inválido. O horário atual está fora do intervalo de funcionamento permitido. " +
-                        "Horário de funcionamento: das " + OPENING_TIME + " às " + CLOSING_TIME + ".");
-
         if (movimentacao.getSaida() != null) {
-            LocalDateTime entrada = movimentacao.getEntrada();
-            LocalDateTime saida = movimentacao.getSaida();
-            Duration duracao = Duration.between(entrada.toLocalTime(), saida.toLocalTime());
-            LocalTime duracaoLocalTime = duracao.toMinutes() >= 0 ? LocalTime.MIDNIGHT.plus(duracao) : LocalTime.MIDNIGHT.minus(duracao);
-            movimentacao.setTempo(duracaoLocalTime);
-        }
 
+            Assert.isTrue(movimentacao.getEntrada().isBefore(movimentacao.getSaida()),
+                    "O tempo de entrada deve ser posterior ao tempo de saída.");
+
+            saidaOperations(movimentacao);
+
+        } else {
+            configurarValoresPadrao(movimentacao);
+        }
     }
 
     /**
@@ -190,5 +123,104 @@ public class MovimentacaoService {
                         "Por favor, verifique se o ID está correto e tente novamente.");
 
     }
+
+    private void validarMovimentacao(Movimentacao movimentacao) {
+
+        // Validar veículo e condutor
+        validarVeiculo(movimentacao.getVeiculo());
+        validarCondutor(movimentacao.getCondutor());
+
+        // Garantir que a entrada não seja nula
+        Assert.notNull(movimentacao.getEntrada(), "A data de entrada da movimentação não foi informada.");
+
+    }
+
+    private void validarCondutor(Condutor condutor) {
+
+        // Garantir que o condutor não seja nulo
+        Assert.notNull(condutor, "O objeto condutor não foi informado.");
+
+        // Garantir que o condutor esteja ativo
+        Assert.isTrue(condutor.isAtivo(), "O condutor associado a essa movimentação está inativo.");
+
+        // Garantir que o ID do condutor não seja nulo
+        Assert.notNull(condutor.getId(), "O ID do condutor em movimentação não pode ser nulo.");
+
+        // Garantir que o condutor exista no repositório
+        Assert.isTrue(condutorRepository.existsById(condutor.getId()),
+                "Não foi possível registrar a movimentação, " +
+                        "o condutor informado não foi encontrado no sistema.");
+
+    }
+
+    private void validarVeiculo(Veiculo veiculo) {
+
+        // Garantir que o veículo não seja nulo
+        Assert.notNull(veiculo, "O objeto veículo não foi informado.");
+
+        // Garantir que o veículo esteja ativo
+        Assert.isTrue(veiculo.isAtivo(), "O veículo associado a essa movimentação está inativo.");
+
+        // Garantir que o ID do veículo não seja nulo
+        Assert.notNull(veiculo.getId(), "O ID do veículo em movimentação não pode ser nulo.");
+
+        // Garantir que o veículo exista no repositório
+        Assert.isTrue(veiculoRepository.existsById(veiculo.getId()),
+                "Não foi possível registrar a movimentação, " +
+                        "o veículo informado não foi encontrado no sistema.");
+
+    }
+
+    private void saidaOperations(Movimentacao movimentacao) {
+
+        Configuracao configuracao = obterConfiguracao();
+
+         LocalTime OPENING_TIME = configuracao.getInicioExpediente();
+         LocalTime CLOSING_TIME = configuracao.getFimExpediente();
+
+        // Obter os valores de entrada, saída e valorMinutoMulta a partir dos objetos movimentacao e obterConfiguracao
+        LocalDateTime entrada = movimentacao.getEntrada();
+        LocalDateTime saida = movimentacao.getSaida();
+        BigDecimal valorMinutoMulta = configuracao.getValorMinutoMulta();
+
+        // Verificar se o horário atual está dentro do horário de funcionamento permitido
+        Assert.isTrue(!LocalTime.from(entrada).isBefore(OPENING_TIME),
+                "Erro: Horário inválido. O horário atual está fora do intervalo de funcionamento permitido. "
+                        + "Horário de funcionamento: das "
+                        + OPENING_TIME + " às "
+                        + CLOSING_TIME + ".");
+
+        // Calcular a duração entre a entrada e a saída
+        Duration duracao = Duration.between(entrada, saida);
+        LocalTime duracaoLocalTime = duracao.toMinutes() >= 0 ? LocalTime.MIDNIGHT.plus(duracao)
+                : LocalTime.MIDNIGHT.minus(duracao);
+        movimentacao.setTempo(duracaoLocalTime);
+
+        if (LocalTime.from(saida).isAfter(CLOSING_TIME)) {
+            // Calcular o tempo para o qual uma multa é aplicável
+            LocalTime tempoMulta = LocalTime.from(saida).minusHours(CLOSING_TIME.getHour())
+                    .minusMinutes(CLOSING_TIME.getMinute())
+                    .minusSeconds(CLOSING_TIME.getSecond())
+                    .minusNanos(CLOSING_TIME.getNano());
+
+            movimentacao.setTempoMulta(tempoMulta);
+            movimentacao.setValorMulta(BigDecimal.valueOf(tempoMulta.getHour())
+                    .multiply(movimentacao.getValorHoraMulta()));
+        } else {
+            configurarValoresPadrao(movimentacao);
+        }
+
+    }
+
+    private void configurarValoresPadrao(Movimentacao movimentacao) {
+
+        // Configurar valores padrão para a movimentação
+        movimentacao.setTempoMulta(LocalTime.MIDNIGHT);
+        movimentacao.setTempo(LocalTime.MIDNIGHT);
+        movimentacao.setValorDesconto(BigDecimal.ZERO);
+        movimentacao.setValorMulta(BigDecimal.ZERO);
+
+    }
+
 
 }

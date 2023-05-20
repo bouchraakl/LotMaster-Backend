18c4eea2163ac08b3efc030e0095d4a9a1163ec5
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-20 18:44:04 -0300

    Spring Validation done for Condutor.java entity

--- src/main/java/br/com/uniamerica/estacionamento/service/CondutorService.java
+++ src/main/java/br/com/uniamerica/estacionamento/service/CondutorService.java
@@ -11,8 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 
 /*
@@ -25,33 +23,16 @@ public class CondutorService {
 
     @Autowired
     private CondutorRepository condutorRepository;
+    @Autowired
+    private MovimentacaoRepository movimentacaoRepository;
 
     /**
      * Valida o cadastro de um novo condutor.
      *
      * @param condutor O condutor a ser validado.
      */
-    @Transactional(readOnly = true, rollbackFor = Exception.class)
+    @Transactional
     public void validarCadastroCondutor(Condutor condutor) {
-
-        // Set data now() na criação da movimentação
-        condutor.setCadastro(LocalDateTime.now());
-
-        // Verifica se o nome do condutor não é nulo
-        Assert.notNull(condutor.getNome(), "O nome do condutor não pode ser nulo.");
-
-        // Verifica se o nome do condutor não é uma string vazia
-        Assert.hasText(condutor.getNome(), "O nome do condutor não pode ser vazio.");
-
-        // Verifica se o nome do condutor contém apenas letras, espaços e hífens
-        final String nomeFormat = "^[a-zA-ZÀ-ÿ\\s\\-]+$";
-        Assert.isTrue(condutor.getNome().matches(nomeFormat),
-                "O nome do condutor deve conter apenas letras, espaços e hífens. " +
-                        "Por favor, verifique e tente novamente.");
-
-        // Verifica o length do nome do condutor
-        Assert.isTrue(condutor.getNome().length() < 40,
-                "O nome do condutor deve ter no máximo 40 caracteres.");
 
         // Verifica se não há nenhum outro condutor com o mesmo CPF cadastrado
         final List<Condutor> condutorbyCPF = this.condutorRepository.findbyCPF(condutor.getCpf());
@@ -59,17 +40,6 @@ public class CondutorService {
                 "Um condutor já está registrado com o CPF informado. " +
                         "Por favor, verifique os dados informados e tente novamente.");
 
-        // Verifica se o CPF não é nulo
-        Assert.notNull(condutor.getCpf(), "O CPF do condutor não pode ser nulo.");
-
-        // Verifica se o CPF não é uma string vazia
-        Assert.hasText(condutor.getCpf(), "O CPF do condutor não pode ser vazio.");
-
-        // Verifica se o CPF está no formato correto
-        final String cpfFormat = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}";
-        Assert.isTrue(condutor.getCpf().matches(cpfFormat) ,
-                "O CPF do condutor deve estar no formato xxx.xxx.xxx-xx " +
-                        "Por favor, verifique e tente novamente.");
 
         // Verifica se o telefone do condutor está no formato correto
         final String telefoneFormat = "\\+55\\(\\d{2}\\)\\d{9}";
@@ -78,17 +48,7 @@ public class CondutorService {
                         "O formato deve seguir o padrão: +55(xx)xxxxxxxxx " +
                         "Por favor, corrija o número de telefone e tente novamente.");
 
-        // Verifica se o telefone do condutor não é nulo
-        Assert.notNull(condutor.getTelefone(), "O telefone do condutor não pode ser nulo.");
-
-        // Verifica se o telefone do condutor não é uma string vazia
-        Assert.hasText(condutor.getTelefone(), "O telefone do condutor não pode ser vazio.");
-
-//        // Set valor de tempoPago
-//        Long totalDurationInSeconds = movimentacaoRepository.sumByTempo(condutor.getId());
-//        LocalTime totalDuration = LocalTime.ofSecondOfDay(totalDurationInSeconds);
-//        condutor.setTempoPago(totalDuration);
-
+        this.condutorRepository.save(condutor);
 
     }
 
@@ -97,11 +57,8 @@ public class CondutorService {
      *
      * @param condutor O condutor a ser validado.
      */
-    @Transactional(readOnly = true, rollbackFor = Exception.class)
+    @Transactional
     public void validarUpdateCondutor(Condutor condutor) {
-
-        // Set data now() na edição da movimentação
-        condutor.setAtualizacao(LocalDateTime.now());
 
         // Verificar se o condutor existe no banco de dados
         Assert.notNull(condutor.getId(),
@@ -113,17 +70,6 @@ public class CondutorService {
                 "O ID do condutor especificado não foi encontrado na base de dados. " +
                         "Por favor, verifique se o ID está correto e tente novamente.");
 
-        // Verifica se o nome do condutor não é nulo
-        Assert.notNull(condutor.getNome(), "O nome do condutor não pode ser nulo.");
-
-        // Verifica se o nome do condutor não é uma string vazia
-        Assert.hasText(condutor.getNome(), "O nome do condutor não pode ser vazio.");
-
-        // Verifica se o nome do condutor contém apenas letras, espaços e hífens
-        final String nomeFormat = "^[a-zA-ZÀ-ÿ\\s\\-]+$";
-        Assert.isTrue(condutor.getNome().matches(nomeFormat),
-                "O nome do condutor deve conter apenas letras, espaços e hífens. " +
-                        "Por favor, verifique e tente novamente.");
 
         // Verifica se não há nenhum outro condutor com o mesmo CPF cadastrado
         final List<Condutor> condutorbyCPF = this.condutorRepository.findbyCPF(condutor.getCpf());
@@ -131,17 +77,6 @@ public class CondutorService {
                 "Um condutor já está registrado com o CPF informado. " +
                         "Por favor, verifique os dados informados e tente novamente.");
 
-        // Verifica se o CPF está no formato correto
-        final String cpfFormat = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}";
-        Assert.isTrue(condutor.getCpf().matches(cpfFormat),
-                "O CPF do condutor deve estar no formato xxx.xxx.xxx-xx " +
-                        "Por favor, verifique e tente novamente.");
-
-        // Verifica se o CPF não é nulo
-        Assert.notNull(condutor.getCpf(), "O CPF do condutor não pode ser nulo.");
-
-        // Verifica se o CPF não é uma string vazia
-        Assert.hasText(condutor.getCpf(), "O CPF do condutor não pode ser vazio.");
 
         // Verifica se o telefone do condutor está no formato correto
         final String telefoneFormat = "\\+55\\(\\d{2}\\)\\d{9}";
@@ -150,16 +85,7 @@ public class CondutorService {
                         "O formato deve seguir o padrão: +55(xx)xxxxxxxxx " +
                         "Por favor, corrija o número de telefone e tente novamente.");
 
-        // Verifica se o telefone do condutor não é nulo
-        Assert.notNull(condutor.getTelefone(), "O telefone do condutor não pode ser nulo.");
-
-        // Verifica se o telefone do condutor não é uma string vazia
-        Assert.hasText(condutor.getTelefone(), "O telefone do condutor não pode ser vazio.");
-
-//        // Set valor de tempoPago
-//        Long totalDurationInSeconds = movimentacaoRepository.sumByTempo(condutor.getId());
-//        LocalTime totalDuration = LocalTime.ofSecondOfDay(totalDurationInSeconds);
-//        condutor.setTempoPago(totalDuration);
+        this.condutorRepository.save(condutor);
 
     }
 
@@ -169,7 +95,7 @@ public class CondutorService {
      * @param id O ID do condutor a ser excluído.
      * @throws IllegalArgumentException caso o ID do condutor não exista no repositório.
      */
-    @Transactional(readOnly = true, rollbackFor = Exception.class)
+    @Transactional
     public void validarDeleteCondutor(Long id) {
 
         // Verifica se ID do condutor existe no banco de dados

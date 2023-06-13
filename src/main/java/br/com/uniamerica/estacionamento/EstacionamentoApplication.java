c6e9703e5668d0f01e878ca25c114faf80474483
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-13 13:50:03 -0300

    Cors Authentication added.

--- src/main/java/br/com/uniamerica/estacionamento/EstacionamentoApplication.java
+++ src/main/java/br/com/uniamerica/estacionamento/EstacionamentoApplication.java
@@ -2,12 +2,21 @@ package br.com.uniamerica.estacionamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class EstacionamentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstacionamentoApplication.class, args);
+	}
+
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:8080")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("Content-Type")
+				.allowCredentials(true);
 	}
 
 }
